0e6186b6babbe13c22846d0eb79ebe6ee92672bb
Ahmed-Bherey <elbhereya@yahoo.com>
2025-09-26 23:05:10 +0300

    categories

--- app/Http/Requests/Store/InventoryUpdateRequest.php
+++ app/Http/Requests/Store/InventoryUpdateRequest.php
@@ -26,27 +26,24 @@ class InventoryUpdateRequest extends FormRequest
     public function rules()
     {
         return [
-            'name'        => 'required|string|max:255',
+            'name'        => 'sometimes|string|max:255',
             'category_id' => 'sometimes|exists:categories,id|integer',
             'img'         => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
             'desc'        => 'nullable|string|max:1000',
-            'price'       => ['required', 'numeric', 'regex:/^\d+(\.0+|\.5)?$/', 'min:0'],
-            'quantity'    => 'required|integer|min:1',
+            'price'       => ['sometimes', 'numeric', 'regex:/^\d+(\.0+|\.5)?$/', 'min:0'],
+            'quantity'    => 'sometimes|integer|min:1',
         ];
     }
 
     public function messages(): array
     {
         return [
-            'name.required'      => 'اسم المنتج مطلوب',
             'category_id.exists' => 'عفوا، التصنيف غير موجود.',
             'img.image'          => 'يجب أن تكون صورة وليست ملف',
             'img.mimes'          => '(jpeg,png,jpg) امتداد الصورة غير مناسب، يجب أن تكون',
             'img.max'            => 'حجم الصورة يجب ألا يزيد عن 2 ميجا',
-            'price.required'     => 'سعر المنتج مطلوب',
             'price.numeric'      => 'السعر يجب أن يكون رقم',
             'price.regex'        => 'السعر يجب أن يكون عددًا صحيحًا أو يحتوي فقط على .5',
-            'quantity.required'  => 'الكمية مطلوبة',
             'quantity.integer'   => 'الكمية يجب أن تكون عددًا صحيحًا',
             'quantity.min'       => 'الكمية يجب أن تكون أكبر من صفر',
         ];
