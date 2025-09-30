943073a512d9c005426e4cb74f15732db6fbd5fc
Ahmed-Bherey <elbhereya@yahoo.com>
2025-09-30 22:11:21 +0300

    track

--- app/Services/Financial/OrderService.php
+++ app/Services/Financial/OrderService.php
@@ -21,7 +21,7 @@ class OrderService
             $query->where('status', 'pending')->where('doctor_id', $user->id);
         } else {
             $query->whereHas('orderItems.product', function ($q) use ($user) {
-                $q->whereIn('status', '!=', 'delivered')->where('user_id', $user->id);
+                $q->where('status', '!=', 'delivered')->where('user_id', $user->id);
             });
         }
 
