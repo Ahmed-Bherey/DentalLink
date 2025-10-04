df0f7d7ad0025f1905037f3f2a5b940142c31388
Ahmed-Bherey <elbhereya@yahoo.com>
2025-10-04 20:14:58 +0300

    payments

--- app/Http/Resources/Financial/PaymentResource.php
+++ app/Http/Resources/Financial/PaymentResource.php
@@ -31,7 +31,7 @@ class PaymentResource extends JsonResource
             'name'          => $name,
             'supplier_name' => $this->supplier->name,
             'paid'          => $this->amount,
-            'remaining'     => $orderExpense->remaining,
+            'remaining'     => $orderExpense->remaining - $this->amount,
             'date'          => $this->date ?? $this->created_at->format('Y-m-d'),
         ];
     }
