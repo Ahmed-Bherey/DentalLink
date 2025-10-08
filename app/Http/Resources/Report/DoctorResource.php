2a07215dfda4729e2f3413a62b360297c22485e8
Ahmed-Bherey <elbhereya@yahoo.com>
2025-10-08 13:57:22 +0300

    reports

--- app/Http/Resources/Report/DoctorResource.php
+++ app/Http/Resources/Report/DoctorResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Report;
 
+use App\Models\Financial\OrderExpense;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class DoctorResource extends JsonResource
@@ -14,12 +15,16 @@ class DoctorResource extends JsonResource
      */
     public function toArray($request)
     {
+        $user = request()->user();
+        $orderExpense = OrderExpense::where(['doctor_id' => $this->id, 'supplier_id' => $user->id])
+            ->latest()->first();
         return [
             'id' => $this->id,
             'name' => $this->name,
             'phone' => $this->phone,
             'phone2' => $this->phone2,
             'address' => $this->address,
+            'remaining' => $orderExpense?->remaining,
         ];
     }
 }
