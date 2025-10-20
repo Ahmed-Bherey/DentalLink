862cbaacdfde49083fe5a2323528d3e70d0195d8
Ahmed-Bherey <elbhereya@yahoo.com>
2025-10-20 22:47:15 +0300

    firebase

--- app/Services/Notifaction/FirebaseService.php
+++ app/Services/Notifaction/FirebaseService.php
@@ -4,46 +4,37 @@ namespace App\Services\Notification;
 
 use Google\Client;
 use GuzzleHttp\Client as HttpClient;
-use Illuminate\Support\Facades\Log;
 
 class FirebaseService
 {
-    protected string $credentialsPath = 'app/firebase/denthub-d6def-firebase-adminsdk-fbsvc-f4af9caf93.json';
-    protected string $projectId = 'denthub-d6def';
-
-    public function send(string $title, string $body, string $token)
+    public function send($title, $body, $token)
     {
-        try {
-            $client = new Client();
-            $client->setAuthConfig(storage_path($this->credentialsPath));
-            $client->addScope('https://www.googleapis.com/auth/firebase.messaging');
+        $client = new Client();
+        $client->setAuthConfig(storage_path('app/firebase/denthub-d6def-firebase-adminsdk-fbsvc-f4af9caf93.json'));
+        $client->addScope('https://www.googleapis.com/auth/firebase.messaging');
 
-            $accessToken = $client->fetchAccessTokenWithAssertion()['access_token'];
+        $http = new HttpClient();
+        $accessToken = $client->fetchAccessTokenWithAssertion()['access_token'];
 
-            $http = new HttpClient();
-            $response = $http->post(
-                "https://fcm.googleapis.com/v1/projects/{$this->projectId}/messages:send",
-                [
-                    'headers' => [
-                        'Authorization' => 'Bearer ' . $accessToken,
-                        'Content-Type'  => 'application/json',
-                    ],
-                    'json' => [
-                        'message' => [
-                            'token' => $token,
-                            'notification' => [
-                                'title' => $title,
-                                'body'  => $body,
-                            ],
+        $response = $http->post(
+            'https://fcm.googleapis.com/v1/projects/denthub-d6def/messages:send',
+            [
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $accessToken,
+                    'Content-Type'  => 'application/json',
+                ],
+                'json' => [
+                    'message' => [
+                        'token' => $token,
+                        'notification' => [
+                            'title' => $title,
+                            'body'  => $body,
                         ],
                     ],
-                ]
-            );
+                ],
+            ]
+        );
 
-            return json_decode($response->getBody(), true);
-        } catch (\Exception $e) {
-            Log::error('Firebase Notification Error: ' . $e->getMessage());
-            return false;
-        }
+        return json_decode($response->getBody(), true);
     }
 }
