2ac7fe5d8ae74a743ce0ad6502e0fd0231c2c387
Ahmed-Bherey <elbhereya@yahoo.com>
2025-10-21 22:50:32 +0300

    one product

--- app/Services/Notifaction/FirebaseService.php
+++ app/Services/Notifaction/FirebaseService.php
@@ -9,15 +9,12 @@ class FirebaseService
 {
     public function send($title, $body, $token)
     {
-        // ضبط الاتصال بخدمة Firebase
         $client = new Client();
         $client->setAuthConfig(storage_path('app/firebase/denthub-d6def-firebase-adminsdk-fbsvc-baf382e3a6.json'));
         $client->addScope('https://www.googleapis.com/auth/firebase.messaging');
 
-        // جلب access token
         $accessToken = $client->fetchAccessTokenWithAssertion()['access_token'];
 
-        // إعداد Guzzle لإرسال الإشعار
         $http = new HttpClient();
         $response = $http->post(
             'https://fcm.googleapis.com/v1/projects/denthub-d6def/messages:send',
