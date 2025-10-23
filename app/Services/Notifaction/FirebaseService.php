4eb95da42b69a6583f934561213a539d4e6e670b
Ahmed-Bherey <elbhereya@yahoo.com>
2025-10-23 23:25:44 +0300

    firebase

--- app/Services/Notifaction/FirebaseService.php
+++ app/Services/Notifaction/FirebaseService.php
@@ -10,14 +10,14 @@ class FirebaseService
     public function send($title, $body, $token)
     {
         $client = new Client();
-        $client->setAuthConfig(storage_path('app/firebase/denthub-d6def-firebase-adminsdk-fbsvc-baf382e3a6.json'));
+        $client->setAuthConfig(storage_path('app/firebase/test-16990-firebase-adminsdk-1fn80-eb97c42188.json'));
         $client->addScope('https://www.googleapis.com/auth/firebase.messaging');
 
         $accessToken = $client->fetchAccessTokenWithAssertion()['access_token'];
 
         $http = new HttpClient();
         $response = $http->post(
-            'https://fcm.googleapis.com/v1/projects/denthub-d6def/messages:send',
+            'https://fcm.googleapis.com/v1/projects/test-16990/messages:send',
             [
                 'headers' => [
                     'Authorization' => 'Bearer ' . $accessToken,
